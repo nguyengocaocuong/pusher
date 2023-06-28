e9233dd4d1cbd1f208832ffea07d53b0f8624a95
Nguyen Ngo Cao Cuong <nguyenngocaocuong@gmail.com>
2023-06-28 13:41:48 +0700

    init project

---
+++ src/main/java/com/pusher/reactjs/realtime/pusher/PusherApplication.java
@@ -0,0 +1,13 @@
+package com.pusher.reactjs.realtime.pusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PusherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PusherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pusher/reactjs/realtime/pusher/PusherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pusher.reactjs.realtime.pusher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PusherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
